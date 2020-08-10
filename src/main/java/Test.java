81406308ff2b4ddea8a3faf5fa18dbc93ca25c33
dail11-sany <96321aaA>
2020-08-10 22:28:32 +0800

    Squashed commit of the following:
    commit 507716c075955961c08cbd0c0da440f6e8265db1
    Author: dail11-sany <96321aaA>
    Date:   Mon Aug 10 22:17:58 2020 +0800
    
        test from dev-child01

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello2");
+        System.out.println("from dev-child01");
     }
 }
